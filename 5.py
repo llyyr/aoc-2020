aae6a263aead2605cd2b1f54ebfcc2fd6dc90c8e
llyyr <llyyr@users.noreply.github.com>
2020-12-06 09:46:52 +0530

    better

--- 5.py
+++ 5.py
@@ -1,19 +1,3 @@
-inp = open('5.in').read().split()
-
-p1 = 0
-seats = []
-
-for line in inp:
-    line = line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
-    row = int(line[:7], 2)
-    column  = int(line[7:], 2)
-    p1 = max(p1, row * 8+column)
-    seats.append(row * 8+column)
-
-print(p1)
-
-
-for i in range(max(seats)):
-    if i-1 in seats and i+1 in seats and i not in seats:
-        print(i)
-
+inp = {int(l.translate({66:49,70:48,76:48,82:49}), 2) for l in open('5.in')}
+print(max(inp))
+print(max({*range(max(inp))}-inp))
